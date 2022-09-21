5c295f5cf0eb61257b07415d7ff614e75797fd14
sujay000 <sujaysapkal22@gmail.com>
2022-09-21 21:33:20 +0530

    33-not-found

--- app.js
+++ app.js
@@ -5,12 +5,18 @@ const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 const path = require('path')
 require('dotenv').config()
+const notFound = require('./middleware/not-found')
 
 // middleware
 app.use(express.static('./public'))
 // or app.use(express.static(path.resolve(__dirname, 'public')))
 app.use(express.json()) // for getting the req.body properly
+
+// routes
 app.use('/api/v1/tasks', tasks)
+
+// if the route is not of the above then it is not found
+app.use(notFound)
 
 const start = async () => {
     try {
---
+++ middleware/not-found.js
@@ -0,0 +1,5 @@
+const notFound = (req, res) => {
+    return res.send('route does not exist')
+}
+
+module.exports = notFound
